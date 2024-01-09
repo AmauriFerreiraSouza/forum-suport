8d8e35edbd034664aad5dd823a645f912108a5e1
Amauri Ferreira de Souza <amauridesouzaferreira@gmail.com>
2024-01-09 16:38:51 -0300

    Creation of the 'create' and 'store' route as well as their methods in the Support control. Created the 'create' view and added its data return table to the 'index' view.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -8,11 +8,21 @@ use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index (Support $support) {
-
+    public function index (Support $support) 
+    {
         $supports = $support->all();
-        dd($supports);
+        //dd($supports);
 
         return view('admin.supports.index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('admin.supports.create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
     }
 }
