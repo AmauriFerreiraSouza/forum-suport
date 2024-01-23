2e147b1bd31ca11e20a5a28fa6ef00dc58eaff36
Amauri Ferreira de Souza <amauridesouzaferreira@gmail.com>
2024-01-23 16:39:09 -0300

    Criada rota 'delete' bem como a função na 'SupportController' 'destroy', criado um botão delete na view 'show'

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -5,3 +5,9 @@
     <li> Status: {{ $support->status }}</li>
     <li> Descrição: {{ $support->body }}</li>
 </ul>
+
+<form action="{{ route('supports.destroy', $support->id) }}" method="POST">
+    @csrf
+    @method('delete')
+<button>Deletar</button>
+</form>
